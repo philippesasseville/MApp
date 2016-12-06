930096e1005f0f14aa95de170606e72dfd4984a2
philippesasseville <philippe.sasseville@polymtl.ca>
2016-12-05 20:19:20 -0500

    click map uk

--- mapp/public/javascripts/d3script.js
+++ mapp/public/javascripts/d3script.js
@@ -28,40 +28,11 @@ d3.json("data/uk.json", function(error, uk) {
 	    .data(topojson.feature(uk, uk.objects.subunits).features)
 	  	.enter().append("path")
 	    .attr("class", function(d) { return "subunit " + d.id; })
-	    .attr("d", path);
-
-	svg.append("path")
-	    .datum(topojson.mesh(uk, uk.objects.subunits, function(a, b) { return a !== b && a.id !== "IRL"; }))
 	    .attr("d", path)
-	    .attr("class", "subunit-boundary");
-
-    svg.append("path")
-	    .datum(topojson.mesh(uk, uk.objects.subunits, function(a, b) { return a === b && a.id === "IRL"; }))
-	    .attr("d", path)
-	    .attr("class", "subunit-boundary IRL");
-
-	svg.append("path")
-		.datum(topojson.feature(uk, uk.objects.places))
-		.attr("d", path)
-		.attr("class", "place");
-
-	svg.selectAll(".place-label")
-		.data(topojson.feature(uk, uk.objects.places).features)
-		.enter().append("text")
-		.attr("class", "place-label")
-		.attr("transform", function(d) { return "translate(" + projection(d.geometry.coordinates) + ")"; })
-		.attr("dy", ".35em")
-		.text(function(d) { return d.properties.name; });
-
-	svg.selectAll(".place-label")
-		.attr("x", function(d) { return d.geometry.coordinates[0] > -1 ? 6 : -6; })
-		.style("text-anchor", function(d) { return d.geometry.coordinates[0] > -1 ? "start" : "end"; });
-
-	svg.selectAll(".subunit-label")
-		.data(topojson.feature(uk, uk.objects.subunits).features)
-		.enter().append("text")
-		.attr("class", function(d) { return "subunit-label " + d.id; })
-		.attr("transform", function(d) { return "translate(" + path.centroid(d) + ")"; })
-		.attr("dy", ".35em")
-		.text(function(d) { return d.properties.name; });
+	    .on("click", function(d){
+	    		if(this.style.fill == "rgb(238, 238, 238)")
+					this.style.fill = "rgb(255, 0, 0)";
+				else
+					this.style.fill = "rgb(238, 238, 238)";
+			});
 });
